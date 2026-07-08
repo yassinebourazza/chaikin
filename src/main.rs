5824b0da00b310adf01766ddf134c2b8c830c367
faaaziz <aazizfatimaa6@gmail.com>
2026-07-08 12:38:48 +0100

    controle reanimation and restart

--- src/main.rs
+++ src/main.rs
@@ -18,19 +18,27 @@ struct Point {
 #[macroquad::main(window_conf)]
 async fn main() {
     let mut points = Vec::new();
-    let last_point : Point ;
+    let mut copy_points = Vec::new();
+
     let mut enterPresed=false;
+    let mut iteration = 0;
+    let mut last_update = 0.0;
+
     loop {
         clear_background(WHITE);
 
-        if is_mouse_button_pressed(MouseButton::Left) {
+        if is_mouse_button_pressed(MouseButton::Left) &&!enterPresed {
             let (x, y) = mouse_position();
             points.push(Point{x,y});
             println!{"{:?}",points};
         }
         
         if is_key_pressed(KeyCode::Escape) {
-            break;
+            enterPresed=false;
+            points = Vec::new();
+            iteration = 0;
+            last_update = 0.0;
+
         }
         
         if is_key_pressed(KeyCode::Backspace) {
@@ -38,7 +46,7 @@ async fn main() {
             println!{"{:?}",points};
         }
        
-          for point in &points {
+         for point in &points {
             draw_circle(point.x, point.y, 3.0, SKYBLUE);
         }
         if !enterPresed{
@@ -47,66 +55,61 @@ async fn main() {
         }
         }
        
-        if is_key_pressed(KeyCode::Enter) ||enterPresed{
-            enterPresed=true;
-            
-            chaikinalgo(&points);
+        if is_key_pressed(KeyCode::Enter) && !enterPresed &&points.len()>0{
+         copy_points=points.clone();
+          enterPresed = true;
+          iteration = 0;
+       }
+       if enterPresed && iteration <= 7{
 
+         if iteration <= 7&& get_time() - last_update > 0.7{
+              copy_points = chaikin_step(&copy_points);
+             iteration += 1;
+            last_update = get_time();
+         }
+           if iteration==7{
+             copy_points=points.clone();
+             iteration=0;
+            }
+     
+         for i in 0..copy_points.len().saturating_sub(1) {
             
-            println!("Enter pressed!");
+             draw_line(
+                 copy_points[i].x,
+                 copy_points[i].y,
+                 copy_points[i + 1].x,
+                 copy_points[i + 1].y,
+                 2.0,
+                 GRAY,
+             );
+         }
         }
-
-       next_frame().await;
+       
+        next_frame().await;
     }
 
 }
-fn chaikinalgo(tab: &Vec<Point>) {
-    let mut res = tab.clone();
+fn chaikin_step(tab: &Vec<Point>) -> Vec<Point> {
+    let mut arr = Vec::new();
 
-    for _ in 0..7 {
-        let mut arr = Vec::new();
+    arr.push(tab[0].clone());
 
-      arr.push(res[0].clone());
-      
-      for i in 0..res.len() - 1 {
-          let p0 = &res[i];
-          let p1 = &res[i + 1];
-      
-          let q = Point {
-              x: 0.75 * p0.x + 0.25 * p1.x,
-              y: 0.75 * p0.y + 0.25 * p1.y,
-          };
-      
-          let r = Point {
-              x: 0.25 * p0.x + 0.75 * p1.x,
-              y: 0.25 * p0.y + 0.75 * p1.y,
-          };
-      
-          arr.push(q);
-          arr.push(r);
-      }
-      
-      arr.push(res[res.len() - 1].clone());
-      
-      res = arr;
+    for i in 0..tab.len() - 1 {
+        let p0 = &tab[i];
+        let p1 = &tab[i + 1];
 
-        clear_background(WHITE);
-          
-        for point in tab {
-            draw_circle(point.x, point.y, 3.0, SKYBLUE);
-        }
+        arr.push(Point {
+            x: 0.75 * p0.x + 0.25 * p1.x,
+            y: 0.75 * p0.y + 0.25 * p1.y,
+        });
 
-           for i in 0..res.len().saturating_sub(1) {
-                 draw_line(
-                  res[i].x,
-                  res[i].y,
-                  res[i + 1].x,
-                  res[i + 1].y,
-                  2.0,
-                  GRAY,
-                  );
-              }
+        arr.push(Point {
+            x: 0.25 * p0.x + 0.75 * p1.x,
+            y: 0.25 * p0.y + 0.75 * p1.y,
+        });
     }
 
- 
+    arr.push(tab[tab.len() - 1].clone());
+
+    arr
 }
