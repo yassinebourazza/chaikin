2f17ed0f7591b1583a3fff16cb3954cba49544c5
faaaziz <aazizfatimaa6@gmail.com>
2026-07-08 12:04:17 +0100

    add chaikin algo

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn window_conf() -> Conf {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug,Clone)]
 struct Point {
     x: f32,
     y: f32
@@ -19,6 +19,7 @@ struct Point {
 async fn main() {
     let mut points = Vec::new();
     let last_point : Point ;
+    let mut enterPresed=false;
     loop {
         clear_background(WHITE);
 
@@ -36,15 +37,76 @@ async fn main() {
             points.clear();
             println!{"{:?}",points};
         }
-        
-        for point in &points {
+       
+          for point in &points {
             draw_circle(point.x, point.y, 3.0, SKYBLUE);
         }
-
+        if !enterPresed{
         for i in 0..points.len().saturating_sub(1) {
             draw_line(points[i].x, points[i].y,points[i + 1].x,points[i + 1].y,2.0,GRAY);
+        }
+        }
+       
+        if is_key_pressed(KeyCode::Enter) ||enterPresed{
+            enterPresed=true;
+            
+            chaikinalgo(&points);
+
+            
+            println!("Enter pressed!");
         }
 
        next_frame().await;
     }
+
+}
+fn chaikinalgo(tab: &Vec<Point>) {
+    let mut res = tab.clone();
+
+    for _ in 0..7 {
+        let mut arr = Vec::new();
+
+      arr.push(res[0].clone());
+      
+      for i in 0..res.len() - 1 {
+          let p0 = &res[i];
+          let p1 = &res[i + 1];
+      
+          let q = Point {
+              x: 0.75 * p0.x + 0.25 * p1.x,
+              y: 0.75 * p0.y + 0.25 * p1.y,
+          };
+      
+          let r = Point {
+              x: 0.25 * p0.x + 0.75 * p1.x,
+              y: 0.25 * p0.y + 0.75 * p1.y,
+          };
+      
+          arr.push(q);
+          arr.push(r);
+      }
+      
+      arr.push(res[res.len() - 1].clone());
+      
+      res = arr;
+
+        clear_background(WHITE);
+          
+        for point in tab {
+            draw_circle(point.x, point.y, 3.0, SKYBLUE);
+        }
+
+           for i in 0..res.len().saturating_sub(1) {
+                 draw_line(
+                  res[i].x,
+                  res[i].y,
+                  res[i + 1].x,
+                  res[i + 1].y,
+                  2.0,
+                  GRAY,
+                  );
+              }
+    }
+
+ 
 }
